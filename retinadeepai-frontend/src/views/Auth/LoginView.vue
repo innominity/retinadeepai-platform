03de7ff70c05109d4be4fcffddeb267cd43de299
Arthur Zhigalov <leroy137.artur@gmail.com>
2025-02-13 15:21:11 +0500

    Add Auth empty views

---
+++ retinadeepai-frontend/src/views/Auth/LoginView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const login = ref('')
+const password = ref('')
+</script>
+
+<template>
+    <section>
+        
+    </section>
+</template>
--- retinadeepai-frontend/src/views/HomeView.vue
+++ retinadeepai-frontend/src/views/HomeView.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <button class="bg-red-300">click me</button>
-  </main>
+  <main></main>
 </template>
